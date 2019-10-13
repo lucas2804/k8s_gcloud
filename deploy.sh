ae0c9887d148375935990d2696bb43cfc0e763ac
Luc Dang <quanglucdang2804@gmail.com>
2019-10-13 13:17:43 +0700

    update travis ci

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t lucdang/multi-client:latest -t lucdang/multi-client -f ./client/Dockerfile ./client
+docker build -t lucdang/multi-server:latest -t lucdang/multi-server -f ./server/Dockerfile ./server
+docker build -t lucdang/multi-worker:latest -t lucdang/multi-worker -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push lucdang/multi-client:latest
+docker push lucdang/multi-server:latest
+docker push lucdang/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+#docker push lucdang/multi-client:$SHA
+#docker push lucdang/multi-server:$SHA
+#docker push lucdang/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+#kubectl set image deployments/server-deployment server=lucdang/multi-server:$SHA
+#kubectl set image deployments/client-deployment client=lucdang/multi-client:$SHA
+#kubectl set image deployments/worker-deployment worker=lucdang/multi-worker:$SHA
